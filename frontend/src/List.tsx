1d8b322458f56d64153c387cdd48e036c3356501
junyoung <no2jfamily@tukorea.ac.kr>
2022-12-07 23:02:29 +0900

    feat: 메인화면 구현

---
+++ frontend/src/List.tsx
@@ -0,0 +1,23 @@
+import styled from 'styled-components';
+import dummy from './db/data.json';
+import React from 'react';
+
+const ListItem = styled.div`
+	padding: 15px;
+	position: relative;
+	background: #fff;
+	border: 1px solid #dee3eb;
+	margin: 15px;
+	cursor: pointer;
+	border-radius: 5px;
+`;
+
+function List() {
+	return (
+		<>
+			<ListItem />
+		</>
+	);
+}
+
+export default List;
